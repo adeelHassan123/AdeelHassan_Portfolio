9455732881a8e29174a103cb886e7f34ed786abc
Adeel <adeel.hassan2k20@gmail.com>
2025-05-10 12:02:20 +0500

    Description Added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -29,9 +29,16 @@ const Hero = () => {
           className="hero-subtitle"
         >
         
-          Deep Learning & Machine Learning Enthusiast | MERN Stack Developer
+          MERN Stack Developer
 
         </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+          className="hero-subtitle">
+            Deep Learning & Machine Learning Enthusiast
+            </motion.p>            
         
         <motion.p
           initial={{ opacity: 0, y: 20 }}
